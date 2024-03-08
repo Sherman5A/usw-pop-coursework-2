2a74dea169cdc946ebcc6d51260120f96342fb6f
Jake <jakereal2004@gmail.com>
2024-03-08 11:21:38 +0000

    Add further comments to CustomerIntegrationTest

--- src/test/java/usw/pop/CustomerIntegrationTest.java
+++ src/test/java/usw/pop/CustomerIntegrationTest.java
@@ -1,10 +1,11 @@
 package usw.pop;
 
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 
@@ -14,10 +15,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 public class CustomerIntegrationTest {
 
     /*
-     Create a temporary database container to test database's interactions with
+     Create a temporary database container to test a database's integration with
      the program
     */
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:126-alpine");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
     Customer customer;
     // CustomerDatabase customerDatabase;
@@ -38,28 +39,43 @@ public class CustomerIntegrationTest {
         postgres.stop();
     }
 
+
+    /**
+     * Connect to the database and proceed to clean it of previous entries
+     */
     @BeforeEach
-    public void cleanDb() {
+    public void setUpCleanDb() {
         // customerDatabase = new CustomerDatabase(postgres.getJbdcUrl());
         // customerDatabase.deleteAll();
     }
 
 
+    /**
+     * Test that {@code customerDatabase} integrates with and adds the customer
+     * to the database
+     */
     @Test
+    @DisplayName("Database create customer")
     public void dbCreateCustomer() {
         customer = new Customer("Jake Real", "jakeemail@email.com", "3 Cool Road, Swandiff");
         // Add the created customer to the database
         // customerDatabase.addCustomer(customer)
         // Test that customer was created in the database
-        // assertEquals("Jake Real", customerDatabase.getCustomerByName("Jake Real").getFullName);
+        // assertEquals("Jake Real", customerDatabase.getCustomerByName("Jake Real").getFullName());
     }
 
+    /**
+     * Test that {@code customerDatabase} integrates with and deletes the
+     * customer from the database
+     */
+    @Test
+    @DisplayName("Database delete customer")
     public void dbDeleteCustomer() {
         customer = new Customer("Jake Real", "jakeemail@email.com", "3 Cool Road, Swandiff");
         // Add the created customer to the database
         // customerDatabase.addCustomer(customer);
         // Test that customer was created in the database
-        // assertEquals("Jake Real", customerDatabase.getCustomerByName("Jake Real").getFullName);
+        // assertEquals("Jake Real", customerDatabase.getCustomerByName("Jake Real").getFullName());
         // Delete the customer from the database
         // customerDatabase.deleteCustomerById(customer.getId);
         // Check that there are no customers
